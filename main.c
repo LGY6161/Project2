17c3bb8fa19a96dd774cd6c5a3b1ea813fb2f334
DESKTOP-CSG4ON0\egois <egoist_0@naver.com>
2023-05-08 23:06:48 +0900

    localtime 함수 호출시 *tm의 return value 유효성에 문제생김.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 struct stat stat1, stat2;
-struct tm *time1, time2;
+struct tm *time1, *time2;
 
 void filestat1(void);
 void filestat2(void);
@@ -15,6 +15,9 @@ void sizecmp(void);
 void blockcmp(void);
 void datecmp(void);
 void timecmp(void);
+
+int file1_mon, file1_day, file1_hour, file1_min;
+int file2_mon, file2_day, file2_hour, file2_min;
 
 int main(void)
 {
@@ -33,7 +36,11 @@ void filetime1()
 {
     printf("Get text1 file time\n");
     time1 = localtime(&stat1.st_mtime);
-    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time1->tm_mon+1, time1->tm_mday, time1->tm_hour, time1->tm_min);
+    file1_mon = time1->tm_mon+1;
+    file1_day = time1->tm_mday;
+    file1_hour = time1->tm_hour;
+    file1_min = time1->tm_min;
+    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",file1_mon, file1_day, file1_hour, file1_min);
 
 }
 
@@ -42,5 +49,9 @@ void filetime2()
 {
     printf("Get text2 file time\n");
     time2 = localtime(&stat2.st_mtime);
-    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time2->tm_mon+1, time2->tm_mday, time2->tm_hour, time2->tm_min);
+    file2_mon = time2->tm_mon+1;
+    file2_day = time2->tm_mday;
+    file2_hour = time2->tm_hour;
+    file2_min = time2->tm_min;
+    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",file2_mon, file2_day, file2_hour, file2_min);
 }
