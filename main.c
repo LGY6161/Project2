5fce0923af9d7fddd199be2de6bb5754d5c151f6
DESKTOP-CSG4ON0\egois <egoist_0@naver.com>
2023-05-08 22:08:04 +0900

    filetime 함수 작성

--- main.c
+++ main.c
@@ -27,3 +27,16 @@ int main(void)
     datecmp();
     timecmp();
 }
+
+void filetime1()
+{
+    time1 = localtime(&stat1.st_mtime);
+    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time1->tm_mon+1, time1->tm_mday, time1->tm_hour, time1->tm_min);
+
+}
+
+void filetime2()
+{
+    time2 = localtime(&stat2.st_mtime);
+    printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time2->tm_mon+1, time2->tm_mday, time2->tm_hour, time2->tm_min);
+}
