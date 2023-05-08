7abb783d9107d6d990f9ba5ca7da53b0087a1a3d
DESKTOP-CSG4ON0\egois <egoist_0@naver.com>
2023-05-08 22:48:54 +0900

    출력문구 추가

--- main.c
+++ main.c
@@ -28,15 +28,19 @@ int main(void)
     timecmp();
 }
 
+// 파일1의 시간 정보를 가져오는 함수
 void filetime1()
 {
+    printf("Get text1 file time\n");
     time1 = localtime(&stat1.st_mtime);
     printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time1->tm_mon+1, time1->tm_mday, time1->tm_hour, time1->tm_min);
 
 }
 
+// 파일2의 시간 정보를 가져오는 함수
 void filetime2()
 {
+    printf("Get text2 file time\n");
     time2 = localtime(&stat2.st_mtime);
     printf("text1 수정시간 : %d월 %d일 %d시 %d분 \n",time2->tm_mon+1, time2->tm_mday, time2->tm_hour, time2->tm_min);
 }
